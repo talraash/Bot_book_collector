78ae4277b1d0d08468b592d7c96eeced2cdcea93
talraash <talraash@gmail.com>
2021-12-29 22:06:22 +0300

    initial commit export nessesary modules

---
+++ book_collector.py
@@ -0,0 +1,14 @@
+import telebot
+import requests as req
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from bs4 import BeautifulSoup
+from datetime import datetime
+from telegram_token import token
+
+
